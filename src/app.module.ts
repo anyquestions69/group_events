9e956c6f682c6d1f75b4fc5663b33b46200003a3
anyquestions69 <danil_tyrenko@icloud.com>
2024-03-12 09:55:35 +0300

    models init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactModule } from './contact/contact.module';
+import { GroupModule } from './group/group.module';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [],
+  imports: [ContactModule, GroupModule, EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
