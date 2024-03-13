93e296be1065ffb4320d784f4480ea84dbbc9564
anyquestions69 <danil_tyrenko@icloud.com>
2024-03-13 13:59:23 +0300

    cd tags

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -2,29 +2,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { TagService } from './tag.service';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { Tag } from '@prisma/client';
 
 @Controller('tag')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
   @Post()
-  create(@Body() createTagDto: CreateTagDto) {
+  create(@Body() createTagDto: Tag) {
     return this.tagService.create(createTagDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.tagService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tagService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto) {
-    return this.tagService.update(+id, updateTagDto);
   }
 
   @Delete(':id')
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { PrismaService } from 'prisma/prisma.service';
+import { Tag } from '@prisma/client';
 
 @Injectable()
 export class TagService {
-  create(createTagDto: CreateTagDto) {
-    return 'This action adds a new tag';
+  constructor(private prisma: PrismaService){}
+  create(createTagDto: Tag) {
+    return this.prisma.tag.create({data:createTagDto});
   }
 
-  findAll() {
-    return `This action returns all tag`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} tag`;
-  }
-
-  update(id: number, updateTagDto: UpdateTagDto) {
-    return `This action updates a #${id} tag`;
-  }
 
   remove(id: number) {
-    return `This action removes a #${id} tag`;
+    return this.prisma.tag.delete({where:{id}});
   }
 }
