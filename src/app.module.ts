818e8c31a35f44d473faed1c035fee836c453afd
anyquestions69 <danil_tyrenko@icloud.com>
2024-03-19 17:12:36 +0300

    started auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,14 @@ import { ContactModule } from './contact/contact.module';
 import { GroupModule } from './group/group.module';
 import { EventModule } from './event/event.module';
 import { TagModule } from './tag/tag.module';
-import { MulterModule } from '@nestjs/platform-express';
 import { MailModule } from './mail/mail.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SchedulerService } from './scheduler/scheduler.service';
 import { SchedulerModule } from './scheduler/scheduler.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ContactModule, GroupModule, EventModule, TagModule, MailModule, ScheduleModule.forRoot(), SchedulerModule],
+  imports: [ContactModule, GroupModule, EventModule, TagModule, MailModule, ScheduleModule.forRoot(), SchedulerModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, SchedulerService],
 })
